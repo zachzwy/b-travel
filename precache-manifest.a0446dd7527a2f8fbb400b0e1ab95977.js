88e2b03adaaba128d4742ba0805cfa2e1ba4c33d
wenyu <zachzwy@gmail.com>
2019-11-17 16:21:38 -0800

    Updates

---
+++ precache-manifest.a0446dd7527a2f8fbb400b0e1ab95977.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c42b1b698ab4b50db4027c8f304e6fb",
+    "url": "/b-travel/index.html"
+  },
+  {
+    "revision": "0c2ea6fdb4f684dfc3a2",
+    "url": "/b-travel/static/css/main.6dcb867e.chunk.css"
+  },
+  {
+    "revision": "9f92eaa2afcf4260ddd1",
+    "url": "/b-travel/static/js/2.dc7e2edd.chunk.js"
+  },
+  {
+    "revision": "0c2ea6fdb4f684dfc3a2",
+    "url": "/b-travel/static/js/main.b9e13bce.chunk.js"
+  },
+  {
+    "revision": "3ee402300d13cec7c62b",
+    "url": "/b-travel/static/js/runtime-main.e57f641a.js"
+  },
+  {
+    "revision": "2b1e7ebbdb0f43e8a147f95b3d589949",
+    "url": "/b-travel/static/media/bg-map.2b1e7ebb.jpg"
+  },
+  {
+    "revision": "5e166b2ede691e86a4b098388c8b4414",
+    "url": "/b-travel/static/media/brex-logo.5e166b2e.svg"
+  },
+  {
+    "revision": "902206909ed2fbf1802f65abd554abbb",
+    "url": "/b-travel/static/media/brex-travel-logo.90220690.svg"
+  }
+]);
